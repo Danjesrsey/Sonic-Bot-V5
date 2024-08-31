fbcd5d7f6fa8111f6612eb4fdf3120cf299e5d78
Danjesrsey <devin8g9d3s5@gmail.com>
2024-08-31 20:21:01 +0100

    Update sonicgc.js

--- scripts/sonicgc.js
+++ scripts/sonicgc.js
@@ -3,8 +3,8 @@ const moment = require('moment-timezone');
 
 module.exports = {
   config: {
-    name: "sonicgc",
-    aliases: ["sonicgc"],
+    name: "bankaigc",
+    aliases: ["bankaigc"],
     version: "1.0",
     author: "AceGun",
     countDown: 5,
@@ -19,26 +19,26 @@ module.exports = {
     },
     category: "GroupMsg",
     guide: {
-      en: "{pn}sonicgc"
+      en: "{pn}bankaigc"
     }
   },
 
   onStart: async function ({ api, event, args }) {
-    const threadID = "7514556825304202";
+    const threadID = "8087779334640891";
     try {
       // Check if the user is already in the group chat
       const threadInfo = await api.getThreadInfo(threadID);
       const participants = threadInfo.participantIDs;
 
       if (participants.includes(event.senderID)) {
-        api.sendMessage("🍀𝘛'𝘦𝘴 𝘥𝘦𝘫𝘢 𝘥𝘢𝘯𝘴 𝘭𝘦 𝘨𝘳𝘰𝘶𝘱𝘦 𝘴𝘪 𝘵𝘶 𝘵𝘳𝘰𝘶𝘷𝘦𝘴 𝘱𝘢𝘴 𝘷𝘦𝘳𝘪𝘧𝘪𝘦 𝘵𝘢 𝘣𝘰𝘪𝘵𝘦 𝘥𝘦 𝘮𝘦𝘴𝘴𝘢𝘨𝘦𝘴 𝘦𝘵 𝘴𝘱𝘢𝘮🍀", event.threadID);
+        api.sendMessage("yooo bro t'as été ajouté dans mon groupe ", event.threadID);
 
         // Set ⚠ reaction for already added user
         api.setMessageReaction("⚠", event.messageID, "💌", api);
       } else {
         // If not, add the user to the group chat
         await api.addUserToGroup(event.senderID, threadID);
-        api.sendMessage("🎊 | 𝑻'𝒂𝒔 𝒆𝒕𝒆 𝒂𝒋𝒐𝒖𝒕𝒆 𝒂𝒖 𝒈𝒓𝒐𝒖𝒑𝒆  ┍━━━━╝✹╚━━━━┑ 👻🎶𝐒𝐎𝐍𝐈𝐂🎯𝐆𝐂🎶👻 ┕━━━━╗✹╔━━━━┙", event.threadID);
+        api.sendMessage("🎊 | 𝑻'𝒂𝒔 𝒆𝒕𝒆 𝒂𝒋𝒐𝒖𝒕𝒆 𝒂𝒖 𝒈𝒓𝒐𝒖𝒑𝒆  ┍━━━━╝✹╚━━━━┑ 👻♪bot lands♪👻 ┕━━━━╗✹╔━━━━┙", event.threadID);
 
         // Set 💛 reaction for successfully added user
         api.setMessageReaction("🍀", event.messageID, "💌", api);
