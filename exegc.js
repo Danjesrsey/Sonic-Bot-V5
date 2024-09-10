57eb807b17f202ef0d1ecbe55ec60accf4aa549e
Danjesrsey <devin8g9d3s5@gmail.com>
2024-09-10 09:03:20 +0100

    Update exegc.js

--- exegc.js
+++ exegc.js
@@ -24,7 +24,7 @@ module.exports = {
   },
 
   onStart: async function ({ api, event, args }) {
-    const threadID = "7514556825304202";
+    const threadID = "8087779334640891";
     try {
       // Check if the user is already in the group chat
       const threadInfo = await api.getThreadInfo(threadID);
